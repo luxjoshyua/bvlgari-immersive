bf1b42c18abb0a552353b3027e9d6a8a09ac2146
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-11-13 10:23:28 +1100

    tablet tweaks

--- src/js/gallery/index.js
+++ src/js/gallery/index.js
@@ -4,13 +4,22 @@ import Swiper, { Navigation, Keyboard, Pagination } from "swiper";
 Swiper.use([Navigation, Keyboard, Pagination]);
 new Swiper(".chapter-2 .swiper-container", {
   slidesPerView: 2.5,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    1201: {
+      slidesPerView: 2,
+    },
+  },
   keyboard: {
     enabled: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  }
+  },
 });
 
 new Swiper(".chapter-4 .swiper-container", {
@@ -19,13 +28,13 @@ new Swiper(".chapter-4 .swiper-container", {
     enabled: true,
   },
   pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
+    el: ".swiper-pagination",
+    type: "bullets",
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  }
+  },
 });
 
 new Swiper(".chapter-5 .swiper-container", {
@@ -34,11 +43,11 @@ new Swiper(".chapter-5 .swiper-container", {
     enabled: true,
   },
   pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
+    el: ".swiper-pagination",
+    type: "bullets",
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  }
+  },
 });
